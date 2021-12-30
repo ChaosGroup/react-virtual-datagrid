b84905d6a02229306282083773f9bc3f50719c21
Hristo Chakarov <mail@ickata.net>
2021-12-30 15:34:30 +0200

    Removing the data reducer util for now

--- src/lib/reduce-data.js
+++
@@ -1,8 +0,0 @@
-export const reduceData = ({ items, total, newItems, offset }) => {
-    items = items.length
-        ? [...items]
-        : Array.from({ length: total });
-
-    items.splice(offset, newItems.length, ...newItems);
-    return items;
-};
